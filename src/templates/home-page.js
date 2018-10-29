85f74352c8af02c64b7f0ee5bc4c0c7be758f49f
Shannon Jensen Wedgwood <shannonjensen@gmail.com>
2018-10-29 23:12:00 +0000

    debug

---
+++ src/templates/home-page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const HomePageTemplate = () => {
+    return (
+        <h1>Home!</h1>
+    )
+}
+
+const HomePage = () => {
+    return (
+        <h1>Home!</h1>
+    )
+}
+
+export default HomePage;
